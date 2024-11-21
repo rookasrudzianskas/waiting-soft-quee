64b9b752ab4ed2c9e0c96c291788d19c900a18b7
rokas <rookas.rudzenskas@gmail.com>
2024-11-21 18:55:35 +0100

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import {ClerkProvider} from "@clerk/nextjs";
+import {ConvexClientProvider} from "@/components/ConvexClientProvider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,9 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <ConvexClientProvider>
         <ClerkProvider>
           {children}
         </ClerkProvider>
+      </ConvexClientProvider>
       </body>
     </html>
   );
